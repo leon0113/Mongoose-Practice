8437c00eb85ad46b67f24a159b9d2f6fe3f81e0c
Leon <tahjib.leon399@gmail.com>
2023-07-25 02:55:54 +0600

    Findone() & fill filtering

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -1,5 +1,5 @@
 import { NextFunction, Request, Response } from "express";
-import { createUserToDb, getUsersFromDb } from "./user.service";
+import { createUserToDb, getUserByIdFromDb, getUsersFromDb } from "./user.service";
 
 export const createUser = async (req: Request, res: Response, next: NextFunction) => {
     const data = req.body;
@@ -11,12 +11,25 @@ export const createUser = async (req: Request, res: Response, next: NextFunction
 };
 
 export const getUsers = async (req: Request, res: Response, next: NextFunction) => {
-    const user = await getUsersFromDb();
+    const users = await getUsersFromDb();
     res.status(200).json({
         status: "success",
-        data: user,
+        data: users,
     });
+    return users;
 };
+
+export const getUserById = async (req: Request, res: Response, next: NextFunction) => {
+    const { id } = req.params;
+
+    const users = await getUserByIdFromDb(id);
+    res.status(200).json({
+        status: "success",
+        data: users,
+    });
+    return users;
+};
+
 
 // Pattern
 //!  route -> controller -> service
--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -1,14 +1,21 @@
 import { IUser } from './user.interface';
 import User from "./user.model";
 
+//! send user to db
 export const createUserToDb = async (payload: IUser): Promise<IUser> => {
-    const user = await new User(payload);
-    await user.save();
+    const user = await new User(payload);    // here User in class and user is a instance of it.
+    await user.save(); // to save in the db  // isntance method
     return user;
 };
-
+//! get all users from db
 export const getUsersFromDb = async (): Promise<IUser[]> => {
     const users = await User.find(); // find every user
     return users;
+}
+
+//! find one user from db
+export const getUserByIdFromDb = async (payload: string): Promise<IUser | null> => {
+    const user = await User.findOne({ id: payload }, { name: 1, contactNo: 1, email: 1, _id: 0 });  // fill filtering -> 1 means I want, 0 means I don't want
+    return user;
 }
 
